450f87e8cfdcbef1cdb5cd26e91278690bfab957
NjokuAnthony <a_njokuchibuzor@yahoo.com>
2023-05-13 21:01:48 +0100

    robots app

---
+++ src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+// import './card.css'
+
+const Card = () => {
+  return (
+    <div className='bg-light-green dib br3 tc pa3 ma2 bw2 grow shadow-5'>
+        <div>
+            <img src="https://robohash.org/tester?200x200" alt="photo" />
+            <h2>Njoku Anthony</h2>
+            <p>a_njokuchibuzor@yahoo.com</p>
+        </div>
+    </div>
+  )
+}
+
+export default Card;
